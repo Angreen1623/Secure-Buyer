0bd221a6254e69bf1cee1b000b9434fd94ca3b06
Bruno-Aparecido-Rodrigues <aparecidobruno749@gmail.com>
2023-09-21 14:55:18 -0300

    Adicionando classes de modelagem e classe de con

---
+++ conteudos/php-conexao-modelagem/perfil.php
@@ -0,0 +1,169 @@
+<?php
+
+include_once 'Conectar.php';
+
+//parte 1 - atributos
+class Perfil
+{
+   private $cod_perfil; 
+   private $nome;
+   private $sobrenome;
+   private $email;
+   private $senha;
+   private $nome_loja;
+   private $cnpj;
+   private $imagem;
+   
+//parte 2 - os getters e setter
+
+public function getcod_perfil(){
+    return $this->cod_perfil;
+}
+
+public function setcod_perfil($codperfil){
+    $this->cod_perfil = $codperfil;
+}
+
+public function getnome(){
+    return $this->nome;
+}
+
+public function setnome($nome_perf){
+    $this->nome = $nome_perf;
+}
+
+public function getsobrenome(){
+    return $this->sobrenome;
+}
+
+public function setsobrenome($sobre_nome){
+    $this->sobrenome = $sobre_nome;
+}
+
+public function getemail(){
+    return $this->email;
+}
+
+public function setemail($e_mail){
+    $this->email = $e_mail;
+}
+
+public function getsenha(){
+    return $this->senha;
+}
+
+public function setsenha($senha_perf){
+    $this->senha = $senha_perf;
+}
+
+public function getnome_loja(){
+    return $this->nome_loja;
+}
+
+public function setnome_loja($nomeloja){
+    $this->nome_loja = $nomeloja;
+}
+
+public function getcnpj(){
+    return $this->cnpj;
+}
+
+public function setcnpj($CNPJ){
+    $this->cnpj = $CNPJ;
+}
+
+public function getimagem(){
+    return $this->imagem;
+}
+
+public function setimagem($imagem_perf){
+    $this->imagem = $imagem_perf;
+}
+
+//salvar
+function salvar()
+{
+    try
+    {
+      $this-> conn = new Conectar();
+      $sql = $this->conn->prepare("insert into perfil values (?,?,?,?,null,null,null,null)");
+      @$sql-> bindParam(1, $this->getnome(), PDO::PARAM_STR);
+      @$sql-> bindParam(2, $this->getsobrenome(), PDO::PARAM_STR);
+      @$sql-> bindParam(3, $this->getemail(), PDO::PARAM_STR);
+      @$sql-> bindParam(4, $this->getsenha(), PDO::PARAM_STR);
+      if($sql->execute() == 1)
+      {
+        echo "<script type='text/javascript'>alert('Registro salvo com sucesso!');</script>";
+      }
+       $this->conn = null;
+    }
+    catch(PDOException $exc)
+    {
+        echo "<script type='text/javascript'>alert('Erro ao salvar o registro: " . $exc->getMessage() . "');</script>";
+    }
+}
+
+//listar
+function listar()
+{
+   try
+   {
+     $this->conn = new Conectar();
+     $sql = $this->conn->query("Select * from perfil order by cod_perfil");
+     $sql->execute();
+     return $sql->fetchAll();
+     $this->conn = null;
+   }
+   catch(PDOException $exc)
+   {
+     echo "Erro ao executar consulta. " . $exc->getMessage();
+   }
+}
+
+//excluir
+
+/*function excluir()
+{
+    try
+    {
+      $this-> conn = new Conectar();
+      $sql = $this->conn->prepare("delete from livro where cod_livro = ?"); //informe o ? (parametro)
+      @$sql-> bindParam(1, $this->getCod_livro(), PDO::PARAM_STR);//essa linha define o parametro
+      if($sql->execute() == 1)
+      {
+        return "<font face = 'Tahoma' color = 'white'><b>Excluído com sucesso!</b></font>";
+      }
+      else
+      {
+        return "Erro na exclusão";
+      }
+       $this->conn = null;
+    }
+    catch(PDOException $exc)
+    {
+      echo "Erro ao excluir " . $exc->getMessage();
+    }
+}*/
+
+//consultar
+function consultar()
+{
+    try
+    {
+      $this-> conn = new Conectar();
+      $sql = $this->conn->prepare("select * from perfil where cod_perfil like ?"); //informe o ? (parametro)
+      @$sql-> bindParam(1, $this->getcod_perfil(), PDO::PARAM_STR);//essa linha define o parametro
+      $sql->execute();
+      return $sql->fetchAll();
+      $this->conn = null;
+    }
+    catch(PDOException $exc)
+    {
+      echo "Erro ao executar consulta " . $exc->getMessage();
+    }
+    
+
+}
+
+}//encerramento da classe produto
+?>
---
+++ conteudos/php-conexao-modelagem/tamanho.php
@@ -0,0 +1,88 @@
+<?php
+
+    include_once 'Conectar.php';
+
+    class size{
+        private $cod_produto;
+        private $size;
+        private $quant_tamanho;
+        private $conn;
+
+        public function getcod_produto(){
+            return $this->cod_produto;
+        }
+
+        public function setcod_produto($ct){
+            $this->cod_produto = $ct;
+        }
+
+        public function getsize(){
+            return $this->size;
+        }
+
+        public function setsize($tam){
+            $this->size = $tam;
+        }
+
+        public function getquant_tamanho(){
+            return $this->quant_tamanho;
+        }
+
+        public function setquant_tamanho($qt){
+            $this->quant_tamanho = $qt;
+        }
+
+        function consultar(){
+            try{
+
+                $this->conn = new Conectar();
+                $sql = $this->conn->prepare("select * from tamanho where cod_produto like ?");
+                @$sql-> bindParam(1, $this->getcod_produto(), PDO::PARAM_INT);
+                $sql->execute();
+                return $sql->fetchAll();
+                $this->conn = null;
+
+            }catch(PDOException $exc){
+                echo "Erro ao executar a consulta.".$exc->getMessage();
+            }
+        }
+
+        
+        function salvar(){
+            try{
+
+                $this->conn = new Conectar();
+                $sql = $this->conn->prepare("insert into tamanho (cod_produto, size, quant_tamanho) values (:codigo,:size,:qt_tamanho)");
+                @$sql-> bindParam(":codigo", $this->getcod_produto(), PDO::PARAM_INT);
+                @$sql-> bindParam(":size", $this->getsize(), PDO::PARAM_STR);
+                @$sql-> bindParam(":qt_tamanho", $this->getquant_tamanho(), PDO::PARAM_INT);
+                if($sql->execute() == 1){
+                    return "Registro salvo com sucesso!";
+                }
+
+            }catch(PDOException $exc){
+                echo "Erro ao salvar o registro.".$exc->getMessage();
+            }
+        }
+
+        function alterar(){
+            try{
+
+                $this->conn = new Conectar();
+                $sql = $this->conn->prepare("update tamanho set size = :size, quant_tamanho = :qt_tamanho where cod_produto = :codigo");
+                @$sql-> bindParam(":size", $this->getsize(), PDO::PARAM_STR);
+                @$sql-> bindParam(":qt_tamanho", $this->getquant_tamanho(), PDO::PARAM_INT);
+                @$sql-> bindParam(":codigo", $this->getcod_produto(), PDO::PARAM_INT);
+                if($sql->execute() == 1){
+                    return "Registro alterado com sucesso!";
+                }
+                $this->conn = null;
+
+            }catch(PDOException $exc){
+                echo "Erro ao alterar o registro.".$exc->getMessage();
+            }
+        }
+
+    }
+
+?>

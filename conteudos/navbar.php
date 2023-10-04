f3693fd8155b60d97c787403bcc4aa9088b02eb9
angreen1623 <andreopaim@gmail.com>
2023-10-04 19:54:44 -0300

    arrumando front

---
+++ conteudos/navbar.php
@@ -0,0 +1,152 @@
+<!-- Inicio da navbar -->
+<header>
+            <!-- início navbar mais a cima -->
+            <div class="topbar">
+
+                <!-- div da lupa c/ seu texto-->
+                <div class="left">
+                    <label>
+                        <!-- icone da lupa -->
+                        <img src="../conteudos/img/lupa.png" alt="">
+                        <!-- texto da lupa -->
+                        <input type="search" placeholder="Pesquisar">
+                    </label>
+                </div>
+                <!-- fim da lupa -->
+
+                <!-- div da logo -->
+                <div class="logo">
+
+                    <!-- se vc clicar na logo volta para o index -->
+                    <a href="../index.php">
+                        <!-- foto da logo -->
+                        <img src="../conteudos/img/logo-sb.png" alt="Icone">
+                    </a>
+
+                </div>
+                <!-- fim da logo -->
+
+                <!-- div dos conteúdos da direita, carrinho e login -->
+                <div class="right">
+
+
+                    <!-- grupo do login -->
+                    <div class="group">
+                        <!-- se clicar no link vai para criar conta -->
+                        <a class="singin-btn" href="<?php
+                            include_once 'php-conexao-modelagem/conexao.php';
+                            $ip = new Conexao();
+
+                            if(!empty($_SERVER['HTTP_CLIENTE_IP'])){
+                                $ip_maquina = $_SERVER['HTTP_CLIENTE_IP'];
+                            }elseif(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
+                                $ip_maquina = $_SERVER['HTTP_X_FORWARDED_FOR'];
+                            }else{
+                                $ip_maquina = $_SERVER['REMOTE_ADDR'];
+                            }
+                            $ip->setendereco_ip($ip_maquina);
+                    
+                            $ips = $ip->listar();
+                    
+                            foreach($ips as $row){
+                                if($ip_maquina = $row['endereco_ip']){
+                                    $codper = $row['cod_perfil'];
+                                }
+                            }
+                        
+                        if(isset($codper)){
+                            echo 'perfil-padrao.php';
+                        }else{ 
+                            echo 'entrar.php';
+                        }?>">
+                            <!-- foto do login -->
+                            <img src="../conteudos/img/user.png" alt="Logar">
+                        </a>
+                    </div>
+                    <!-- fim do grupo do login -->
+
+                    <!-- grupo do carrinho -->
+                    <div class="group">
+                        <!-- se clicar no link abre o carrinho -->
+                        <a id="bag-btn" class="bag-btn" href="#">
+                            <!-- imagem da sacola -->
+                            <img src="../conteudos/img/bag.png" alt="Sacola">
+                        </a>
+                    </div>
+                    <!-- fim do grupo -->
+
+                </div>
+                <!-- div do grupo -->
+
+            </div>
+            <!-- fim da barra superior -->
+
+            
+
+
+            <div class="bag">
+            <span class="close-icon"><img src="../conteudos/img/close.png" alt="fechar"></span>
+            <div class="bag-sidebar">
+                <div class="bag-title">
+                    <h4>Meu carrinho</h4>
+                </div>
+                <div class="bag-body">
+                    <div class="bag-item">
+                        <div class="img">
+                            <img src="../conteudos/img/modelo15.png" alt="">
+                        </div>
+                        <div class="text">
+                            <div class="bag-info">
+                                <h5>Jaqueta</h5>
+                                <h3>MIUMIU</h3>
+                            </div>
+                            <h5 class="bag-price">R$350,00</h5>
+                            <span>- 1 +</span>
+                            <div class="right">
+                                <img src="../conteudos/img/del.png" alt="deletar">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="bag-item">
+                        <div class="img">
+                            <img src="../conteudos/img/modelo16.png" alt="">
+                        </div>
+                        <div class="text">
+                            <div class="bag-info">
+                                <h5>Jaqueta</h5>
+                                <h3>RIACHUELO</h3>
+                            </div>
+                            <h5 class="bag-price">R$115,00</h5>
+                            <span>- 1 +</span>
+                            <div class="right">
+                                <img src="../conteudos/img/del.png" alt="deletar">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="bag-buy">
+
+                        <div class="group cupom">
+                            <img src="../conteudos/img/cupom.png" alt="">
+                            <h3>Adicionar cupom de desconto</h3>
+                        </div>
+                        <div class="itens">
+                            <h4 class="left">Frete:</h4>
+
+                            <div class="group right">
+                                <img src="../conteudos/img/frete.png" alt="">
+                                <h3 class="underline">Calcular</h3>
+                            </div>
+                        </div>
+
+                        <div class="itens">
+                            <h4 class="left">Total:</h4>
+                            <h3 class="right">R$465,00</h3>
+                        </div>
+                        <div class="bag-end">
+                            <span class="btn">Finalizar compra</span>
+                        </div>
+                        <span class="continue underline">Continuar comprando</span>
+                    </div>
+                </div>
+            </div>
+        </div>

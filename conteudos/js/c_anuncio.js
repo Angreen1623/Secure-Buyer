9d842b7c8c50b4021dcf80e5a6d1bcd8bbad5753
angreen1623 <andreopaim@gmail.com>
2023-10-18 23:21:38 -0300

    imagens se alteram ao adicionar um file

--- conteudos/js/c_anuncio.js
+++ conteudos/js/c_anuncio.js
@@ -1,21 +1,52 @@
-var more_btn = document.querySelector('.mais');
-var less_btn = document.querySelector('.menos');
-var quantitys = document.querySelector('#qnt');
+// var more_btn = document.querySelector('.mais');
+// var less_btn = document.querySelector('.menos');
+// var quantitys = document.querySelector('#qnt');
 
-more_btn.onclick = function(){
-    quantity.value++;
-    verTamanho();
+// more_btn.onclick = function(){
+//     quantity.value++;
+//     verTamanho();
+// };
+
+// less_btn.onclick = function(){
+//     quantity.value--;
+//     verTamanho();
+// };
+
+// function verTamanho(){
+//     for(i=1; i<=6;i++){
+//         if(quantity.value >= Math.pow(10, i)-1){
+//             quantity.style.width = i+1+"7px";
+//         }
+//     }
+// }                                            Não ta funcionando, n sei arrumar ainda
+
+var img1 = document.querySelector('.img1');
+const inputArquivo1 = document.querySelector("#imagem1");
+
+var img2 = document.querySelector('.img2');
+const inputArquivo2 = document.querySelector("#imagem2");
+
+var img3 = document.querySelector('.img3');
+const inputArquivo3 = document.querySelector("#imagem3");
+
+inputArquivo1.onchange = function () { //Função para atualizar a interface do usuário quando um arquivo é selecionado.
+
+  if (inputArquivo1.files.length > 0) {  //Verifica se há um arquivo selecionado.
+    img1.src = URL.createObjectURL(inputArquivo1.files[0]); // Cria uma URL temporária para o arquivo selecionado e atualiza a imagem.
+  }
+
 };
+inputArquivo2.onchange = function () { //Função para atualizar a interface do usuário quando um arquivo é selecionado.
 
-less_btn.onclick = function(){
-    quantity.value--;
-    verTamanho();
+  if (inputArquivo2.files.length > 0) {  //Verifica se há um arquivo selecionado.
+    img2.src = URL.createObjectURL(inputArquivo2.files[0]); // Cria uma URL temporária para o arquivo selecionado e atualiza a imagem.
+  }
+
 };
+inputArquivo3.onchange = function () { //Função para atualizar a interface do usuário quando um arquivo é selecionado.
 
-function verTamanho(){
-    for(i=1; i<=6;i++){
-        if(quantity.value >= Math.pow(10, i)-1){
-            quantity.style.width = i+1+"7px";
-        }
-    }
-}
+  if (inputArquivo3.files.length > 0) {  //Verifica se há um arquivo selecionado.
+    img3.src = URL.createObjectURL(inputArquivo3.files[0]); // Cria uma URL temporária para o arquivo selecionado e atualiza a imagem.
+  }
+
+};

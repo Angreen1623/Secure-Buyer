1d4135abd5583d51e82023368b50bace0a74bacf
AndreiNune <142557578+AndreiNune@users.noreply.github.com>
2023-11-10 01:59:52 -0300

    Create sobrenos.php

---
+++ docs/conteudos/sobrenos.php
@@ -0,0 +1,49 @@
+<!DOCTYPE php>
+<php lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Secure Buyer</title>
+    <!--Logo-->
+    <link rel="apple-touch-icon" sizes="180x180" href="img/Icone/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="img/Icone/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="img/Icone/favicon-16x16.png">
+    <link rel="manifest" href="img/Icone/site.webmanifest">
+    <!--css-->
+    <link rel="stylesheet" href="./css/reset.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/sobrenos.css">
+    
+</head>
+
+<body>
+    <?php include 'navbar.php'; ?>
+
+    <div class="wrapper">
+
+        <div class="title">
+            <h1>SOBRE NÓS</h1>
+        </div>
+
+        <div class="content">
+
+            <div class="box">
+
+            <h2>Como surgimos?</h2>
+            
+                <p>Nós surgimos com o pensamento e objetivo de ajudar pequenas lojas que não possuem os meios nem as ferramentas de divulgação a par com os dias de hoje! 
+                Então, o que nos torna diferente da Amazon? Foi o que você pensou certo? Bom, além de divulgarmos, somente lojas de roupas imagine uma situação onde 
+                você precisa de uma peça em específico para terminar um conjunto espetacular, quer ele seja para uma reunião formal, um casamento, uma festa ou até 
+                mesmo um evento de cosplay. Mas tenha em mente que sempre estamos visando a segurança de sua compra!</p>
+
+            </div>
+
+        </div>
+         
+    </div>
+    <?php include 'footer.php'; ?>
+ 
+</body>
+<!--javascript-->
+<script src="js/script.js"></script>
+</php>
